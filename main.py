cb85c7bc1f3f85fe57eae3fe4ad50eb7a7aaa76f
pubudu lasith <pubudu093@gmail.com>
2023-12-29 09:37:24 +1100

    commit

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from sensor.configuration.mogo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os, sys
+from sensor.logger import logging
+
+def test_exception():
+    try:
+        logging.info('we are dividing 1 by 0')
+        x= 1/0
+    except Exception as e:
+        raise SensorException(e, sys)
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print('pubudu')
-    print(mongodb_client.database.list_collection_names())
+    # mongodb_client = MongoDBClient()
+    # print('pubudu')
+    # print(mongodb_client.database.list_collection_names())
+
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+        
+
