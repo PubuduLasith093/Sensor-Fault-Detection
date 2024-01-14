6d485a816e1ae09d794fa2d8e33dc0ab2ce83b31
pubudu lasith <pubudu093@gmail.com>
2024-01-14 15:03:52 +1100

    added

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -15,15 +15,17 @@ from sensor.cloud_storage.s3_syncer import S3Sync
 from sensor.constants.s3_bucket import TRAINING_BUCKET_NAME
 from sensor.constants.training_pipeline import SAVED_MODEL_DIR
 
-class TrainingPipeline:
+class TrainPipeline:
+    is_pipeline_running=False
     def __init__(self):
-        training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config = training_pipeline_config)
-        self.trainig_pipeline_config = training_pipeline_config
+        self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
+        
+
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
-            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.trainig_pipeline_config)
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Starting data ingestion")
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
@@ -31,10 +33,10 @@ class TrainingPipeline:
             return data_ingestion_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
-        
+
     def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
         try:
-            data_validation_config = DataValidationConfig(training_pipeline_config=self.trainig_pipeline_config)
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
             data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
             data_validation_config = data_validation_config
             )
@@ -42,10 +44,10 @@ class TrainingPipeline:
             return data_validation_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
-        
+
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
-            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.trainig_pipeline_config)
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
             data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
             data_transformation_config=data_transformation_config
             )
@@ -53,10 +55,10 @@ class TrainingPipeline:
             return data_transformation_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
-        
+    
     def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact):
         try:
-            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.trainig_pipeline_config)
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
             model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
             model_trainer_artifact = model_trainer.initiate_model_trainer()
             return model_trainer_artifact
@@ -67,7 +69,7 @@ class TrainingPipeline:
                                  model_trainer_artifact:ModelTrainerArtifact,
                                 ):
         try:
-            model_eval_config = ModelEvaluationConfig(self.trainig_pipeline_config)
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
             model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
             model_eval_artifact = model_eval.initiate_model_evaluation()
             return model_eval_artifact
@@ -76,7 +78,7 @@ class TrainingPipeline:
 
     def start_model_pusher(self,model_eval_artifact:ModelEvaluationArtifact):
         try:
-            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.trainig_pipeline_config)
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
             model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
             model_pusher_artifact = model_pusher.initiate_model_pusher()
             return model_pusher_artifact
@@ -96,11 +98,11 @@ class TrainingPipeline:
             self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
         except Exception as e:
             raise SensorException(e,sys)
-        
+
     def run_pipeline(self):
         try:
             
-            TrainingPipeline.is_pipeline_running=True
+            TrainPipeline.is_pipeline_running=True
 
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
@@ -110,11 +112,10 @@ class TrainingPipeline:
             if not model_eval_artifact.is_model_accepted:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
-            TrainingPipeline.is_pipeline_running=False
+            TrainPipeline.is_pipeline_running=False
             self.sync_artifact_dir_to_s3()
             self.sync_saved_model_dir_to_s3()
         except  Exception as e:
             self.sync_artifact_dir_to_s3()
-            TrainingPipeline.is_pipeline_running=False
+            TrainPipeline.is_pipeline_running=False
             raise  SensorException(e,sys)
-        
